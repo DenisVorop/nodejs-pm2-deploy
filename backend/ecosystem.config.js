11cde36d03e64a46afe218966f29b06fa54c8d62
DenisVorop <saskeazaske@icloud.com>
2025-01-15 14:58:39 +0300

    ci: add pm2 for backend

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'api-service',
+      script: './dist/app.js',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:DenisVorop/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
